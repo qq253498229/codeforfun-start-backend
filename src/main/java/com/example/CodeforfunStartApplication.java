407801be02b7ddc36f530b330493ddb237c2e1c2
大胃王 <253498229@qq.com>
2018-11-23 15:53:36 +0800

    initial commit

---
+++ src/main/java/com/example/CodeforfunStartApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeforfunStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeforfunStartApplication.class, args);
+	}
+}
